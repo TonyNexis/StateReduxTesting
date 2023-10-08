4b7b320396b68f72f2763a7a0804a0a125c4192c
Anton <tony.zenekis@gmail.com>
2023-10-09 00:53:42 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { Provider } from 'react-redux'
+
+import store from './redux/store';
+import Likes from './likes';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrap">
-        <div className="card">
-          <div className="card-image">
-            <img src="./sea.jpg" alt="surfing"/>
-            <div>Hello World</div>
+      <div className="App">
+        <div className="wrap">
+          <div className="card">
+            <div className="card-image">
+              <img src="./sea.jpg" alt="surfing"/>
+              <Likes store={store}/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
