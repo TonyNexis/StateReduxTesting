75a379c5833dfa2978c71cf91b6831e0ad2cc353
Anton <tony.zenekis@gmail.com>
2023-10-09 23:04:06 +0300

    test commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,2 @@
-import { createStore } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
-    likes: 0
-}
-
-const reducer = (state = initialState, action) => {
-    console.log('reducer >', action)
-
-    return state;
-}
-
-const store = createStore(reducer);
-
-export default store;
